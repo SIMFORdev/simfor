f605789fa7fac40864ecb097cdc26e8164448a2f
flitch <dimabro180@gmail.com>
2023-11-29 01:10:13 +0300

    initial commit

---
+++ examples/SlauTest/GaussOmpExe.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "simfor/GaussOmp.hpp"
+
+simfor::matr genMatNMB(int n){
+        simfor::matr m(n, n+1);
+        for(auto i=0;i<n;i++){
+            for(auto j=0;j<n+1;j++){
+                if (i==j)
+                {
+                    m(i,j) = 10*n*fabsf64x(rand()%100+11);
+                }else{
+                    m(i,j) = rand()%10;
+                }
+            }
+        }
+        return m;
+}
+
+int main(int argc, char const *argv[])
+{
+    const auto N = 4096;
+
+    //Наша исходная матрица и вектор свободных чисел 
+    std::vector<simfor::vec> myMatB = {{32, 2, 1, 3, 1},
+                    {1, 8, 3, 1, 3},
+                    {1, 2, 16, 3, 1},   
+                    {1, 2, 3, 56, 1},  
+                    {1, 2, 1, 3, 32}};
+    simfor::vec myVecB = {43, 14, -3, 169, -13};
+    
+    //Матрица вида Ab необходима для работы функции GaussianElimination
+    // simfor::matr myMatA(N, N+1);
+
+    // //Копируем значения
+    // for (auto i = 0; i < myMatB.size(); i++){
+    //     for (auto j = 0; j < myMatB[i].size(); j++){
+    //         myMatA(i, j) = myMatB[i][j];
+    //     }
+    //     myMatA(i, N) = myVecB[i];
+    // }
+
+    simfor::matr myMatA = genMatNMB(N);
+
+    simfor::vec resVec = simfor::GaussianElimination(myMatA, N);
+
+    std::cout << "Answer: " << [resVec](){ for (auto &&i : resVec){std::cout << i << " ";}; return "\n";}();
+
+    return 0;
+}
+
