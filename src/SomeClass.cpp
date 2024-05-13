a62aa69face033e05ce730620fe19ef4afe5ad03
Vadim Tsoy <tsoy086.90@mail.ru>
2024-05-13 18:55:58 +0300

    develop

---
+++ src/SomeClass.cpp
@@ -0,0 +1,15 @@
+//
+// Created by vadim on 19.09.23.
+//
+
+#include "simfor/SomeClass.hpp"
+
+#include <iostream>
+
+namespace simfor {
+
+void SomeClass::HelloWorld() {
+	std::cout << "Hello world\n";
+}
+
+}
